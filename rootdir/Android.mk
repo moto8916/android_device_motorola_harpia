c2444f3d697290950ff7fd9a6771b7fc79721bc9
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-09-03 01:36:57 -0400

    Use prebuilt kernel for now
    Change-Id: Ib21bf267a11d0a694cf12a031bdf893f33522b2f

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,3 +9,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := module_hashes
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/module_hashes
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
